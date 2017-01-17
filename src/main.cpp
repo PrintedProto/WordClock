0130d35d15ee0a0808cc1fdc202cd3137d71d18d
Joseffus Santos <joseffus.santos@gmail.com>
2017-01-16 18:48:54 -0600

    Wifi Manager implemented

--- src/main.cpp
+++ src/main.cpp
@@ -11,28 +11,32 @@
 // select wich pin will trigger the configuraton portal when set to LOW
 // ESP-01 users please note: the only pins available (0 and 2), are shared
 // with the bootloader, so always set them HIGH at power-up
-#define TRIGGER_PIN 8
+#define wifimgr_PIN D7
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("\n Starting");
-
-  pinMode(TRIGGER_PIN, INPUT);
+  //Serial.println("\n Starting");
+  //int pinstatus;
+  pinMode(wifimgr_PIN, INPUT);
+  //pinstatus = digitalRead(TRIGGER_PIN);
+  //Serial.println("PinStatus= ");
+  //Serial.print(pinstatus);
 }
 
 
 void loop() {
+
   // is wifi config requested?
-  if ( digitalRead(TRIGGER_PIN) == LOW ) {
+  if ( digitalRead(wifimgr_PIN) == LOW ) {
     //WiFiManager
     WiFiManager wifiManager; //Local intialization. Once its business is done, there is no need to keep it around
     //wifiManager.resetSettings();//reset settings - for testing
     //wifiManager.setTimeout(120); //sets timeout until configuration portal gets turned off useful to make it all retry or go to sleep in seconds
     //WiFi.mode(WIFI_STA); //WITHOUT THIS THE AP DOES NOT SEEM TO WORK PROPERLY WITH SDK 1.5 , update to at least 1.5.1
 
-    if (!wifiManager.startConfigPortal("OnDemandAP")) {
+    if (!wifiManager.startConfigPortal("AP_Proto")) {
       Serial.println("failed to connect and hit timeout");
       delay(3000);
       ESP.reset();//reset and try again, or maybe put it to deep sleep
@@ -41,6 +45,7 @@ void loop() {
 
     Serial.println("connected...yeey :)");//if you get here you have connected to the WiFi
   }
+  //end wifimgrsetupmode
 
 
   // put your main code here, to run repeatedly:
